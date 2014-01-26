2bf4d68889a1ddde49536dd6bcd86dc6a0abb657
sjahl <stephenjahl@gmail.com>
2014-01-26 10:22:54 -0500

    initial commit

---
+++ emacs.d/plugins/copy-line.el
@@ -0,0 +1,7 @@
+(provide 'copy-line)
+(defun copy-line (arg)
+      "Copy lines (as many as prefix argument) in the kill ring"
+      (interactive "p")
+      (kill-ring-save (line-beginning-position)
+                      (line-beginning-position (+ 1 arg)))
+      (message "%d line%s copied" arg (if (= 1 arg) "" "s")))
---
+++ linker.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+ln -si `pwd`/bash/bashrc ~/.bashrc
+ln -si `pwd`/bash/bash_profile ~/.bash_profile
+ln -si `pwd`/screen-tmux/screenrc ~/.screenrc
+ln -si `pwd`/screen-tmux/tmux.conf ~/.tmux.conf
+ln -si `pwd`/zsh/zshrc ~/.zshrc
+ln -si `pwd`/emacs.d ~/.emacs.d
