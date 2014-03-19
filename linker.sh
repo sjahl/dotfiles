57de8d22dfced2bf43ecea4eeba8ae19a55711db
jahl <jahl@csail.mit.edu>
2014-03-19 15:02:59 -0400

    some cleanup, adding ace-jump locally because the one on marmalade is sad, and commiting some local changes

--- linker.sh
+++ linker.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
-ln -s ${PWD}/bash/bashrc $HOME/.bashrc
-ln -s ${PWD}/bash/bash_profile $HOME/.bash_profile
-ln -s ${PWD}/screen-tmux/screenrc $HOME/.screenrc
-ln -s ${PWD}/screen-tmux/tmux.conf $HOME/.tmux.conf
-ln -s ${PWD}/zsh/zshrc $HOME/.zshrc
-ln -s ${PWD}/emacs.d $HOME/.emacs.d
-ln -s ${PWD}/vim/vimrc $HOME/.vimrc
+ln -si ${PWD}/bash/bashrc $HOME/.bashrc
+ln -si ${PWD}/bash/bash_profile $HOME/.bash_profile
+ln -si ${PWD}/screen-tmux/screenrc $HOME/.screenrc
+ln -si ${PWD}/screen-tmux/tmux.conf $HOME/.tmux.conf
+ln -si ${PWD}/zsh/zshrc $HOME/.zshrc
+ln -si ${PWD}/emacs.d $HOME/.emacs.d
+ln -si ${PWD}/vim/vimrc $HOME/.vimrc
