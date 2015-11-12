6c7e63c9c0ace6297acb54aa7a725dacb4146c90
Stephen Jahl <jahl@csail.mit.edu>
2015-11-12 16:29:57 -0500

    need to think about this more for OSX

--- emacs.d/init.el
+++ emacs.d/init.el
@@ -21,11 +21,6 @@
 ;; tls trust checking
 (setq tls-checktrust 'ask)
 
-(setq tls-program
-      '("gnutls-cli --x509cafile /etc/ssl/certs/ca-certificates.crt -p %p %h"
-	"gnutls-cli --x509cafile /etc/ssl/certs/ca-certificates.crt -p %p %h --protocols ssl3"
-	"openssl s_client -connect %h:%p -CAfile /etc/ssl/certs/ca-certificates.crt -no_ssl2 -ign_eof"))
-
 ;; package archives
 (require 'package)
 (setq package-archives 
@@ -42,7 +37,7 @@
 ;;       (load-theme 'sanityinc-tomorrow-night t)))
 
 ;; colors
-(load-theme 'base16-default-dark t)
+(load-theme 'base16-atelierheath-dark t)
 
 ;; keybindings
 (global-set-key (kbd "M--") 'er/expand-region)
