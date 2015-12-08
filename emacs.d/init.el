937b027e7e409693807eec9b06f8422643a14298
Stephen Jahl <sjahl@quantopian.com>
2015-12-08 17:05:52 -0500

    trying the full path to flake8

--- emacs.d/init.el
+++ emacs.d/init.el
@@ -49,4 +49,4 @@
 ;; flymake
 (require 'flymake-python-pyflakes)
 (add-hook 'python-mode-hook 'flymake-python-pyflakes-load)
-(setq flymake-python-pyflakes-executable "flake8")
+(setq flymake-python-pyflakes-executable "/usr/local/bin/flake8")
