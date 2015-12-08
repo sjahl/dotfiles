e750354ec71fcf984c84103e8b5c1727e2edcd08
Stephen Jahl <sjahl@quantopian.com>
2015-12-08 16:59:24 -0500

    adding pyflakes stuff to emacs ini

--- emacs.d/init.el
+++ emacs.d/init.el
@@ -45,3 +45,8 @@
 (require 'copy-line)
 (define-key global-map (kbd "C-c k") 'kill-whole-line)
 (define-key global-map (kbd "C-c C-k") 'copy-line)
+
+;; flymake
+(require 'flymake-python-pyflakes)
+(add-hook 'python-mode-hook 'flymake-python-pyflakes-load)
+(setq flymake-python-pyflakes-executable "flake8")
