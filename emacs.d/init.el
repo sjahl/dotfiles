bf6dace9ccd51c40141ffed62c8eb916a89c987f
Stephen Jahl <sjahl@quantopian.com>
2016-01-15 14:36:28 -0500

    trying a more explicit flake8

--- emacs.d/init.el
+++ emacs.d/init.el
@@ -41,9 +41,25 @@
 (global-set-key (kbd "C-c C-<") 'mc/mark-all-like-this)
 
 ;; flymake
-(require 'flymake-python-pyflakes)
-(add-hook 'python-mode-hook 'flymake-python-pyflakes-load)
-(setq flymake-python-pyflakes-executable "/usr/local/bin/flake8")
+;; (require 'flymake-python-pyflakes)
+;; (add-hook 'python-mode-hook 'flymake-python-pyflakes-load)
+;; (setq flymake-python-pyflakes-executable "/usr/local/bin/flake8")
+
+(require 'flymake)
+(setq flymake-max-parallel-syntax-checks nil)  ;; Run on all my buffers
+(when (load "flymake" t)
+  (defun flymake-pyflakes-init ()
+    (let* ((temp-file (flymake-init-create-temp-buffer-copy
+                       'flymake-create-temp-inplace))
+           (local-file (file-relative-name
+                        temp-file
+                        (file-name-directory buffer-file-name))))
+      (list (executable-find "flake8") (list local-file))))
+  (add-to-list 'flymake-allowed-file-name-masks
+               '("\\.py\\'" flymake-pyflakes-init)))
+
+(add-hook 'find-file-hook 'flymake-find-file-hook)
+
 
 
 ;; get path from shell
