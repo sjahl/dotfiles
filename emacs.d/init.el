eef387caa05089b668bd60d84eba8e9145f525bd
Stephen Jahl <sjahl@quantopian.com>
2016-01-15 14:42:14 -0500

    no difference, sticking with the simpler config

--- emacs.d/init.el
+++ emacs.d/init.el
@@ -41,26 +41,9 @@
 (global-set-key (kbd "C-c C-<") 'mc/mark-all-like-this)
 
 ;; flymake
-;; (require 'flymake-python-pyflakes)
-;; (add-hook 'python-mode-hook 'flymake-python-pyflakes-load)
-;; (setq flymake-python-pyflakes-executable "/usr/local/bin/flake8")
-
-(require 'flymake)
-(setq flymake-max-parallel-syntax-checks nil)  ;; Run on all my buffers
-(when (load "flymake" t)
-  (defun flymake-pyflakes-init ()
-    (let* ((temp-file (flymake-init-create-temp-buffer-copy
-                       'flymake-create-temp-inplace))
-           (local-file (file-relative-name
-                        temp-file
-                        (file-name-directory buffer-file-name))))
-      (list (executable-find "flake8") (list local-file))))
-  (add-to-list 'flymake-allowed-file-name-masks
-               '("\\.py\\'" flymake-pyflakes-init)))
-
-(add-hook 'find-file-hook 'flymake-find-file-hook)
-
-
+(require 'flymake-python-pyflakes)
+(add-hook 'python-mode-hook 'flymake-python-pyflakes-load)
+(setq flymake-python-pyflakes-executable "/usr/local/bin/flake8")
 
 ;; get path from shell
 (when (memq window-system '(mac ns))
