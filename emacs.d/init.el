b4fd0496ca92a3683f85c9503237c0b3fd78a02a
sjahl <sjahl@quantopian.com>
2016-01-21 09:31:29 -0500

    ocean

--- emacs.d/init.el
+++ emacs.d/init.el
@@ -31,7 +31,7 @@
 
 (if window-system
     (progn
-      (load-theme 'base16-twilight-dark t)))
+      (load-theme 'base16-ocean-dark t)))
 
 ;; general keybindings
 (global-set-key (kbd "M--") 'er/expand-region)
