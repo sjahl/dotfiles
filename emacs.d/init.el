1cd2459efc412f20fffe9d3b38ce6deff9387560
sjahl <sjahl@quantopian.com>
2016-01-21 09:26:33 -0500

    more theme testing blah

--- emacs.d/init.el
+++ emacs.d/init.el
@@ -31,7 +31,7 @@
 
 (if window-system
     (progn
-      (load-theme 'base16-tomorrow-dark t)))
+      (load-theme 'base16-twilight-dark t)))
 
 ;; general keybindings
 (global-set-key (kbd "M--") 'er/expand-region)
