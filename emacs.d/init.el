e85dba7cd19bb48e5a872a89a73d76af37f5f286
Stephen Jahl <sjahl@quantopian.com>
2016-03-24 09:27:18 -0400

    more eighties

--- emacs.d/init.el
+++ emacs.d/init.el
@@ -29,7 +29,7 @@
 
 (if window-system
     (progn
-      (load-theme 'sanityinc-tomorrow-night t)))
+      (load-theme 'base16-eighties-dark t)))
 
 ;; general keybindings
 (global-set-key (kbd "M--") 'er/expand-region)
