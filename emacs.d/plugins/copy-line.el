d318c05a2450349e93cee9a8a2a60bee3503d500
Stephen Jahl <sjahl@quantopian.com>
2018-07-10 14:47:42 -0400

    cleanup and sync

--- emacs.d/plugins/copy-line.el
+++
@@ -1,7 +0,0 @@
-(provide 'copy-line)
-(defun copy-line (arg)
-      "Copy lines (as many as prefix argument) in the kill ring"
-      (interactive "p")
-      (kill-ring-save (line-beginning-position)
-                      (line-beginning-position (+ 1 arg)))
-      (message "%d line%s copied" arg (if (= 1 arg) "" "s")))
