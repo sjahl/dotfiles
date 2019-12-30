396afd05e90c49feed94a85c739b6c3e7ae14cf9
Stephen Jahl <steve@jahl.net>
2019-12-30 16:33:19 -0500

    i don't use this anymore

--- linker.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-ln -si ${PWD}/bash/bashrc $HOME/.bashrc
-ln -si ${PWD}/bash/bash_profile $HOME/.bash_profile
-ln -si ${PWD}/screen-tmux/screenrc $HOME/.screenrc
-ln -si ${PWD}/screen-tmux/tmux.conf $HOME/.tmux.conf
-ln -si ${PWD}/zsh/zshrc $HOME/.zshrc
-ln -si ${PWD}/emacs.d $HOME/.emacs.d
-ln -si ${PWD}/vim/vimrc $HOME/.vimrc
